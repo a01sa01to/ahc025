84cc8a12e468e3a12dac7970010255ce9bda62cc
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-10-15 07:35:05 +0000

    [1st sub] initial

--- avg.py
+++ avg.py
@@ -16,8 +16,8 @@ for i in range(1, FILES + 1):
         flg = False
         lines = f.readlines()
         for line in lines:
-            # if line.count("Score ="):
-            if line.count("Cost ="):
+            if line.count("Score ="):
+            # if line.count("Cost ="):
                 t = int(line.split(" = ")[1])
                 flg = True
                 if t:
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use proconio::{input, source::line::LineSource};
+use std::io::{stdin, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let mut source = LineSource::new(BufReader::new(stdin.lock()));
+
+    input! {
+        from &mut source,
+        n: usize,
+        d: usize,
+        q: usize,
+    };
+
+    let mut ans = vec![0; n];
+    for i in 0..n {
+        ans[i] = i % d;
+    }
+
+    for _ in 0..q {
+        println!("{} {} {} {}", 1, 1, 0, 1);
+        input! {
+            from &mut source,
+            _res: char,
+        };
+    }
+
+    for i in 0..n {
+        print!("{} ", ans[i]);
+    }
+    println!();
 }
